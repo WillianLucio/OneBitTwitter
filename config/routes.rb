bf4f4250f198e578f6766f807cf8ded01fec851c
willian <willian.l.lucio@gmail.com>
2018-11-30 10:56:54 -0200

    Including authentication in API - JWT.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      post 'user_token', to: 'user_token#create'
+    end
+  end
 end
+# "jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDQxODcyOTIsInN1YiI6Mn0.rxf42WIerDPjNGJxPhw0PZg7SBYDNBq6gxTAlNPPDpo"
