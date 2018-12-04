fdc2fecaf2c4e4b52138c6c1bb532511b0aa6364
willian <willian.l.lucio@gmail.com>
2018-12-04 10:07:07 -0200

    Adjusting serializer and the like controller

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,10 +1,10 @@
 module Api
   module V1
     class TweetsController < Api::V1::ApiController
-      before_action :set_current_user
+      before_action { current_user }
       before_action :set_tweet, except: %i[create index]
-      before_action :authenticate_user, except: %i[show create]
-      load_and_authorize_resource except: %i[index show create]
+      before_action :authenticate_user, except: %i[show index]
+      load_and_authorize_resource except: %i[index show]
 
       def index
         user = User.find params[:user_id]
@@ -18,12 +18,12 @@ module Api
         if @tweet.save
           render json: @tweet, status: :created
         else
-          render json: @tweet.errors, status: :unprocessable_entity
+          render json: { errors: @tweet.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def show
-        render json: @tweet, include: '**'
+        render json: @tweet
       end
 
       def destroy
@@ -31,10 +31,10 @@ module Api
       end
 
       def update
-        if @tweet.update(tweet_params.merge(user: current_user))
+        if @tweet.update(tweet_params)
           render json: @tweet
         else
-          render json: @tweet.errors, status: :unprocessable_entity
+          render json: { errors: @tweet.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
@@ -46,10 +46,6 @@ module Api
 
       def tweet_params
         params.require(:tweet).permit(:body, :tweet_original_id)
-      end
-
-      def set_current_user
-        @current_user = current_user || nil
       end
     end
   end
